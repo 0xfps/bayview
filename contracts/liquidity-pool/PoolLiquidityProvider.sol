dac7281cf5a18726c85620c7f0ede4a66f00ee8a
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-10-30 08:57:39 +0100

    feat: liquidity provider contract.

--- contracts/liquidity-pool/PoolLiquidityProvider.sol
+++ contracts/liquidity-pool/PoolLiquidityProvider.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
+import { INonFungiblePositionManager } from "./interfaces/INonFungiblePositionManager.sol";
+
 import { PoolCreator } from "./PoolCreator.sol";
+
+contract PoolLiquidityProvider is PoolCreator {
+    constructor (address _nonFungiblePositionManager)
+        PoolCreator(_nonFungiblePositionManager) {}
+
+    function _addLiquidity(INonFungiblePositionManager.MintParams memory mintParams) internal {
+        nonFungiblePositionManager.mint(mintParams);
+    }
+}
